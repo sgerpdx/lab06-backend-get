c6a871ed22ac120004b0d0b8be3d7bde3eb94573
SamuelHGerber <henrylightfoot@gmail.com>
2021-02-18 14:06:48 -0800

    get and listen validated by sending to localhost and logging to terminal

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const { app } = require('./app.js');
 const port = process.env.PORT || 3000;
+const cors = require('cors');
 
 
 //console.log('fully installed and operational');
 
 app.use(cors())
+
+app.listen(port, () => {
+    console.log(`Spoiler: app was already listening at http://localhost:${port}`)
+});
+
+
