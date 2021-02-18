a7ad5a4246dbc6867636adc7fa8ea176fbdf79e8
SamuelHGerber <henrylightfoot@gmail.com>
2021-02-18 15:59:10 -0800

    gets added back to app.js

--- __tests__/get.test.js
+++ __tests__/get.test.js
@@ -4,25 +4,6 @@ const supertest = require('supertest');
 const { planets } = require('../data.js');
 const request = supertest(app);
 
-// it('should return a response inlcuding all planets', async done => {
-//     const planetsData = [
-//         {
-//             'id': 1,
-//             'planet': 'mercury',
-//             'class': 'terrestrial',
-//             'diameter': 4880,
-//             'gravity': 0.4,
-//             'magnetic_field_strong': false,
-//         }
-//     ]
-
-//     const response = await request.get('/planets')
-
-//     expect(response.status).toBe(200)
-//     expect(response.body).toEqual({ results: planets })
-//     done()
-// })
-
 
 it('should return a response inlcuding all planets', async done => {
     const expectation = {
@@ -30,6 +11,26 @@ it('should return a response inlcuding all planets', async done => {
     };
 
     const response = await request.get('/planets');
+
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual(expectation);
+    done();
+});
+
+
+it('should return a response of only the selected planet', async done => {
+    const expectation = {
+        results: {
+            'id': 1,
+            'planet': 'mercury',
+            'class': 'terrestrial',
+            'diameter': 4880,
+            'gravity': 0.4,
+            'magnetic_field_strong': false,
+        }
+    };
+
+    const response = await request.get('/planets/1');
 
     expect(response.status).toBe(200);
     expect(response.body).toEqual(expectation);
