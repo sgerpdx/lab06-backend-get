7bd657ca1430ac7494acfdc26ae0243a915773cc
SamuelHGerber <henrylightfoot@gmail.com>
2021-02-18 15:45:39 -0800

    package.json update

--- __tests__/get.test.js
+++ __tests__/get.test.js
@@ -1,23 +1,37 @@
 const { app } = require('../app.js');
 const supertest = require('supertest');
-const { request } = require('express');
-const requrest = supertest(app);
+//const { request } = require('express');
+const { planets } = require('../data.js');
+const request = supertest(app);
 
-it('gets the test endpoint', async done => {
-    const planetsData = [
-        {
-            'id': 1,
-            'planet': 'mercury',
-            'class': 'terrestrial',
-            'diameter': 4880,
-            'gravity': 0.4,
-            'magnetic_field_strong': false,
-        }
-    ]
+// it('should return a response inlcuding all planets', async done => {
+//     const planetsData = [
+//         {
+//             'id': 1,
+//             'planet': 'mercury',
+//             'class': 'terrestrial',
+//             'diameter': 4880,
+//             'gravity': 0.4,
+//             'magnetic_field_strong': false,
+//         }
+//     ]
 
-    const response = await request.get('/test')
+//     const response = await request.get('/planets')
 
-    expect(response.status).toBe(554)
-    expect(response.body.message).toEqual(planetsData)
-    done()
-})
+//     expect(response.status).toBe(200)
+//     expect(response.body).toEqual({ results: planets })
+//     done()
+// })
+
+
+it('should return a response inlcuding all planets', async done => {
+    const expectation = {
+        results: planets
+    };
+
+    const response = await request.get('/planets');
+
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual(expectation);
+    done();
+});
--- app.js
+++ app.js
@@ -5,14 +5,6 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 
-app.get('/', (req, res) => {
-    res.json({ greeting: 'Hello World!' })
-})
-
-app.get('/astronomy', (req, res) => {
-    res.json({ robot_message: 'comet-incoming' })
-})
-
 app.get('/planets', (req, res) => {
     res.json({ results: planets })
 })
